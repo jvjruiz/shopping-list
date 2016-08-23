2319e80940aa9533cfff044affa5ae2d44a51622
jvjruiz <jovincent.ruiz@gmail.com>
2016-08-23 11:30:38 -0700

    final

--- js/app.js
+++ js/app.js
@@ -1,21 +1,48 @@
-// type something in shopping list entry and press button to add to list 
 
-// make a box with class shopping item name and buttons
 
-// when adding to list create check and delete button
+
+
+
 
 //when click check, highlight box aka change color of box
 
-//when click delete, get rid of box 
+ 
+
 
 $(document).ready(function() {
 
-	$('.js-shopping-list-form').click('.button') {
-	console.log('button pressed');
-	//$('.shopping-item').css('background-color:', 'teal')
-	};
 
 
+// make box with class shopping item name and buttons
+	$("form").submit(function(event) {
+		var userInput = $("#shopping-list-entry").val()
+		event.preventDefault();
+		$('ul').append('<li>'
+			+ "<span class='shopping-item'>"
+			+ userInput 
+			+ "</span>" 
+			+ "<div class='shopping-item-controls'>" 
+			+ "<button class='shopping-item-toggle'>" 
+			+ "<span class='button-label'>" 
+			+ "check" + "</span>" +"</button>"
+			+ " "
+			+ "<button class='shopping-item-delete'>" 
+			+ " <span class='button-label'>" 
+			+ "delete" 
+			+ "</span>" 
+			+ "</button>" )
+		userInput = ""
+		$('#shopping-list-entry').val(userInput)
+	})
+//when click delete, get rid of box
+	$(".shopping-list").on('click', '.shopping-item-delete', function(event) {
+  		$(this).closest('li').remove();
+  	});
+
+//when click check, crosses list item
+	$(".shopping-list").on('click', '.shopping-item-toggle', function(event) {
+		$(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+	});
 
 
 });
