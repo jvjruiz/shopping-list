aaf20da6b9681cbc0a16b86e1aca42268ac3bfea
jvjruiz <jovincent.ruiz@gmail.com>
2016-08-23 12:00:54 -0700

    completed main assignment

--- js/app.js
+++ js/app.js
@@ -1,45 +1,33 @@
-
-
-
-
-
-
 //when click check, highlight box aka change color of box
-
- 
-
-
 $(document).ready(function() {
-
-
 
 // make box with class shopping item name and buttons
 	$("form").submit(function(event) {
 		var userInput = $("#shopping-list-entry").val()
 		event.preventDefault();
 		$('ul').append('<li>'
-			+ "<span class='shopping-item'>"
-			+ userInput 
-			+ "</span>" 
+			+ "<span class='shopping-item'>" + userInput + "</span>" 
 			+ "<div class='shopping-item-controls'>" 
 			+ "<button class='shopping-item-toggle'>" 
-			+ "<span class='button-label'>" 
-			+ "check" + "</span>" +"</button>"
+			+ "<span class='button-label'>" + "check" + "</span>" 
+			+ "</button>"
 			+ " "
 			+ "<button class='shopping-item-delete'>" 
-			+ " <span class='button-label'>" 
-			+ "delete" 
-			+ "</span>" 
-			+ "</button>" )
+			+ " <span class='button-label'>" + "delete" + "</span>" 
+			+ "</button>" 
+			)
 		userInput = ""
 		$('#shopping-list-entry').val(userInput)
 	})
+
 //when click delete, get rid of box
+
 	$(".shopping-list").on('click', '.shopping-item-delete', function(event) {
   		$(this).closest('li').remove();
   	});
 
 //when click check, crosses list item
+
 	$(".shopping-list").on('click', '.shopping-item-toggle', function(event) {
 		$(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
 	});
