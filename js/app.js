e657208fe8d89df4e83d4c9444910a1ed32cbfd9
jvjruiz <jovincent.ruiz@gmail.com>
2016-08-23 08:27:13 -0700

    first

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// type something in shopping list entry and press button to add to list 
+
+// make a box with class shopping item name and buttons
+
+// when adding to list create check and delete button
+
+//when click check, highlight box aka change color of box
+
+//when click delete, get rid of box 
+
+$(document).ready(function() {
+
+	$('.js-shopping-list-form').click('.button') {
+	console.log('button pressed');
+	//$('.shopping-item').css('background-color:', 'teal')
+	};
+
+
+
+
+});
